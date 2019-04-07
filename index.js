10f4051d3a2de5d4479f05d05f0d49522a857f12
jonatan-reseck <jonatanreseck@yahoo.com.br>
2019-04-07 00:28:13 -0300

    conectar ao zeit

--- index.js
+++ index.js
@@ -9,10 +9,18 @@ const dbConnection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), { Prom
 
 const port = process.env.PORT || 3000
 
+app.use('/admin', (req, res, next)=>{
+    if(req.hostname === 'localhost'){
+        next()
+    }else{
+        res.send('Not Allowed')
+    }
+})
+
 app.set('views', path.join(__dirname, 'views'))
 //faz o ejs tratar a formatação da pagina web
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
+app.use(express.static(path.join(__dirname,'public')))
 app.use(bodyParser.urlencoded({ extended: true}))
 
 app.get('/', async (request, response) => {
