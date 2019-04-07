6b3b4b6a9708ad5678eb00d38d9ed8c991d38a2b
jonatan-reseck <jonatanreseck@yahoo.com.br>
2019-04-07 00:10:55 -0300

    Forcing Update :)

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 
+const path = require('path')
+
 const sqlite = require('sqlite')
-const dbConnection = sqlite.open('banco.sqlite', { Promise })
+const dbConnection = sqlite.open(path.resolve(__dirname, 'banco.sqlite'), { Promise })
 
 const port = process.env.PORT || 3000
 
+app.set('views', path.join(__dirname, 'views'))
 //faz o ejs tratar a formatação da pagina web
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
